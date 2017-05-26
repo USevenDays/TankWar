6c0b767297647ae80d48a28a0bdc6cceb15ec653
xingjing17 <13572904021@163.com>
2017-05-26 09:59:59 +0800

    测试工程上传备份

---
+++ Assets/TestScp.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using System;
+//[ExecuteInEditMode]
+public class TestScp : MonoBehaviour {
+
+    Action ad;
+    // Use this for initialization
+    void Start () {
+        print("back");
+        DontDestroyOnLoad(gameObject);
+    }
+	
+	// Update is called once per frame
+	public void Onclick() {
+        SceneManager.LoadScene(1);
+        print("jumpManager");
+    }
+    public void Onclick2()
+    {
+        SceneManager.LoadScene(0);
+        print("jump");
+    }
+}
